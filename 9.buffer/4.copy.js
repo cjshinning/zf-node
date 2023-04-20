8b74a9406d3c4aa11084319a7f352a41143c1562
cjshinningg <784319417@qq.com>
2023-04-20 11:25:36 +0800

    拷贝实现

--- 9.buffer/4.copy.js
+++ 9.buffer/4.copy.js
@@ -21,4 +21,42 @@ const path = require('path');
 // 权限chomo -R 777 二爷一直4读书 r（读） 4 w（写） 2 x（执行） 1
 const buffer = Buffer.from('架构');
 
-fs.open(path.resolve(__dirname, 'xxx.js'), 'w', 0o666)
+// node中采用了留的方式简化了这些代码 解耦 发布订阅模式 来实现解耦操作
+function copy(source, target, cb) {
+  const BUFFER_LENGTH = 3;
+  let read_position = 0;
+  let write_position = 0;
+  const buffer = Buffer.alloc(BUFFER_LENGTH);
+  fs.open(source, 'r', function (err, rfd) {
+    fs.open(target, 'w', function (err, wfd) {
+      function next() {
+        fs.read(rfd, buffer, 0, BUFFER_LENGTH, read_position, function (err, bytesRead) { //读取到的实际个数
+          if (err) return cb(err);
+          if (bytesRead) {
+            // 读出来再写进去
+            fs.write(wfd, buffer, 0, bytesRead, write_position, function (err, written) {
+              read_position += bytesRead;
+              write_position += bytesRead;
+              next();
+            })
+          } else {
+            fs.close(rfd, () => { });
+            fs.close(wfd, () => { });
+            cb();
+          }
+
+        })
+      }
+      next();
+    })
+  })
+}
+copy(path.resolve(__dirname, '3.fs.js'), path.resolve(__dirname, 'xxx.js'), function () {
+  console.log('copy success');
+})
+
+// fs.open(path.resolve(__dirname, 'xxx.js'), 'w', function (err, fd) {
+//   fs.write(fd, buffer, 0, 3, 0, function (err, written) {  //written写入个数
+
+//   })
+// })
